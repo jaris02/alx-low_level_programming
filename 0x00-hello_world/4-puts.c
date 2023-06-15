93329298ffa08854ad9262dd4223d55b18666536
jaris02 <zizuasmat@gmail.com>
2023-06-15 20:47:09 +0300

    new commit all in one

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints a statment only by using puts function..
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char *s = "\"Programming is like building a multilingual puzzle";
+puts(s);
+return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - A function that prints a line using printf.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
